57182123ce938025926d9f79d22cf68b0b2b22b2
aditya8sharma <adityasharmacom1000@gmail.com>
2024-02-14 18:33:16 +0530

    Terraform 101

---
+++ input.tf
@@ -0,0 +1,22 @@
+terraform{
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>4.16"
+        }
+    }
+    required_version = ">=1.2.0"
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+    ami = "ami-008fe2fc65df48dac"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Terraform_Demo"
+    }
+}
